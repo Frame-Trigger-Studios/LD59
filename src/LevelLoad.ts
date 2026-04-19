0d768cf3514c67b7b4ed4eacbece94cc766abcd1
Peter Mandile <peterm1994@hotmail.com>
2026-04-19 16:59:06 +1000

    removed old debug stuff

--- src/LevelLoad.ts
+++ src/LevelLoad.ts
@@ -27,19 +27,14 @@ export class Tile extends Entity {
             yAnchor: 0.5
         }));
 
-        const debug = false;
-
         switch (this.tileId) {
             case 4:
-                if (debug) this.addComponent(new RenderPoly([[0, 8], [8, -8], [8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[0, 8], [8, -8], [8, 8]]}));
                 break;
             case 5:
-                if (debug) this.addComponent(new RenderPoly([[-8, -8], [0, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, -8], [0, 8], [-8, 8]]}));
                 break;
             case 7:
-                if (debug) this.addComponent(new RenderRect({width: 16, height: 16, yOff: -8, xOff: -8}));
                 this.addComponent(new RectSatCollider({
                     layer: Layers.SOLIDS,
                     xOff: -8,
@@ -49,99 +44,81 @@ export class Tile extends Entity {
                 }));
                 break;
             case 8:
-                if (debug) this.addComponent(new RenderPoly([[0, -8], [8, -8], [8, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[0, -8], [8, -8], [8, 8], [-8, 8]]
                 }));
                 break;
             case 9:
-                if (debug) this.addComponent(new RenderPoly([[-8, -8], [0, -8], [8, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[-8, -8], [0, -8], [8, 8], [-8, 8]]
                 }));
                 break;
             case 10:
-                if (debug) this.addComponent(new RenderPoly([[8, -8], [8, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[8, -8], [8, 8], [-8, 8]]}));
                 break;
             case 11:
-                if (debug) this.addComponent(new RenderPoly([[-8, -8], [8, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, -8], [8, 8], [-8, 8]]}));
                 break;
             case 12:
-                if (debug) this.addComponent(new RenderPoly([[0, 8], [8, 8], [8, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[0, 8], [8, 8], [8, -8], [-8, -8]]
                 }));
                 break;
             case 13:
-                if (debug) this.addComponent(new RenderPoly([[-8, 8], [0, 8], [8, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[-8, 8], [0, 8], [8, -8], [-8, -8]]
                 }));
                 break;
             case 14:
-                if (debug) this.addComponent(new RenderPoly([[8, 8], [8, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[8, 8], [8, -8], [-8, -8]]}));
                 break;
             case 15:
-                if (debug) this.addComponent(new RenderPoly([[-8, 8], [8, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, 8], [8, -8], [-8, -8]]}));
                 break;
             case 16:
-                if (debug) this.addComponent(new RenderPoly([[0, -8], [8, 8], [8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[0, -8], [8, 8], [8, -8]]}));
                 break;
             case 17:
-                if (debug) this.addComponent(new RenderPoly([[-8, 8], [0, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, 8], [0, -8], [-8, -8]]}));
                 break;
             case 20:
-                if (debug) this.addComponent(new RenderPoly([[-8, 8], [8, 0], [8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, 8], [8, 0], [8, 8]]}));
                 break;
             case 21:
-                if (debug) this.addComponent(new RenderPoly([[-8, 0], [8, -8], [8, 8], [-8, 8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[-8, 0], [8, -8], [8, 8], [-8, 8]]
                 }));
                 break;
             case 22:
-                if (debug) this.addComponent(new RenderPoly([[8, 0], [-8, -8], [-8, 8], [8, 8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[8, 0], [-8, -8], [-8, 8], [8, 8]]
                 }));
                 break;
             case 23:
-                if (debug) this.addComponent(new RenderPoly([[8, 8], [-8, 0], [-8, 8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[8, 8], [-8, 0], [-8, 8]]}));
                 break;
             case 24:
-                if (debug) this.addComponent(new RenderPoly([[-8, -8], [8, 0], [8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[-8, -8], [8, 0], [8, -8]]}));
                 break;
             case 25:
-                if (debug) this.addComponent(new RenderPoly([[-8, 0], [8, 8], [8, -8], [-8, -8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[-8, 0], [8, 8], [8, -8], [-8, -8]]
                 }));
                 break;
             case 26:
-                if (debug) this.addComponent(new RenderPoly([[8, 0], [-8, 8], [-8, -8], [8, -8]]));
                 this.addComponent(new PolySatCollider({
                     layer: Layers.SOLIDS,
                     points: [[8, 0], [-8, 8], [-8, -8], [8, -8]]
                 }));
                 break;
             case 27:
-                if (debug) this.addComponent(new RenderPoly([[8, -8], [-8, 0], [-8, -8]]));
                 this.addComponent(new PolySatCollider({layer: Layers.SOLIDS, points: [[8, -8], [-8, 0], [-8, -8]]}));
                 break;
 
